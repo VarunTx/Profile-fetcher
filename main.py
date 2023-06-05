13cb7b66b6adb39ef3951f3bc826dfc921c3dd51
kushagrapandeyy <70605582+kushagrapandeyy@users.noreply.github.com>
2023-06-05 20:28:49 +0530

    Added main.py and modified readme

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def login():
     s = requests.Session()
     rv = s.get(URL + '/uas/login?trk=guest_homepage-basic_nav-header-signin')
     p = BeautifulSoup(rv.content, "html.parser")
-
     print(p)
 
 login()
